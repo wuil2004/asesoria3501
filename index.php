22e7a447d1bec3f7f24338319c60abf310015053
williams <williamsmatiassan@gmail.com>
2024-10-11 03:09:17 -0600

    modificacion

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mi primera aplicacion con PHP</title>
+    <title>MI PRIMERA APLICACION CON PHP</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <h1>Soy un encabezado de html</h1>
