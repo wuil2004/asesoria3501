2418d20b7fe83712da22cf35e875e37401ffdecc
williams <williamsmatiassan@gmail.com>
2024-10-11 04:54:23 -0600

    cambios

--- pagina2.php
+++ pagina2.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="css/estilo2.css">
 </head>
 <body>
-    <section class="content_form">
+    <section class="content-form">
 
         <h1>FOMULARIOS CON PHP</h1>
 
         <form action="procesar_form.php" method="post">
-            <input type="text" placeholder="Nombre" name="txtNombre">
-            <input type="number" placeholder="Edad" name="txtEdad">
+            <input type="text" placeholder="Nombre" name="txtNombre" class="cajas-texto">
+            <input type="number" placeholder="Edad" name="txtEdad" class="cajas-texto">
 
             <select size="1" name="seleccioneCarrera" class="opcion">
                 <option>--Seleccione Carrera</option>
@@ -24,10 +24,8 @@
                 <option value="civil">ing civil</option>
             </select>
 
-            <div class="botones">
-                <button>Registrar</button>
-                <button><Nav>Nuevo</Nav></button>
-            </div>
+            <button class="botones">Registrar</button>
+            <button class="botones"><Nav>Nuevo</Nav></button>
         </form>
     </section>
 
--- procesar_form.php
+++ procesar_form.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Procesar de datos del formulario</title>
+    <link rel="stylesheet" href="css/estilo3.css">
 </head>
 <body>
-    <?php
-        //recibir los datos por post
-        $nombre = $_POST["txtNombre"];
+    <section class="content-form">
+        <?php
+            //recibir los datos por post
+            $nombre = $_POST["txtNombre"];
 
-        echo "<h1>tu nombre es $nombre</h1>"
-    ?>
+            echo "<h1>tu nombre es $nombre</h1>"
+        ?>
+    </section>
     
 </body>
 </html>
